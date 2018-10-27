6412dc8c27b85fadda4601222a3deaad62178574
garrett <garrett7621@gmail.com>
2018-10-27 13:14:39 -0400

    state races

---
+++ app/src/main/java/com/example/c_89983/voting_iupui/state_house.java
@@ -0,0 +1,14 @@
+package com.example.c_89983.voting_iupui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class state_house extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.state_house_can);
+
+    }
+}
