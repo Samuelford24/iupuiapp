3e096d084d3e85ccf367e56514373ff5f93f3c1f
C-89983 <c-89983@in-info-271-25m.soic.iupui.edu>
2018-10-27 12:00:31 -0400

    init commit

---
+++ app/src/main/java/com/example/c_89983/voting_iupui/county_races.java
@@ -0,0 +1,12 @@
+package com.example.c_89983.voting_iupui;
+
+import android.app.AppComponentFactory;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class county_races extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.county);
+    }
+}
---
+++ app/src/main/java/com/example/c_89983/voting_iupui/senate_canditates.java
@@ -0,0 +1,12 @@
+package com.example.c_89983.voting_iupui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class senate_canditates extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.senate_canditates);
+    }
+}
