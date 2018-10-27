82a3950b69de46393e83b0d760b942808e8042c0
C-89983 <c-89983@in-info-271-25m.soic.iupui.edu>
2018-10-27 12:47:48 -0400

    partial

---
+++ app/src/main/java/com/example/c_89983/voting_iupui/house_canditates.java
@@ -0,0 +1,13 @@
+package com.example.c_89983.voting_iupui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class house_canditates extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.house_canditates);
+
+    }
+}
