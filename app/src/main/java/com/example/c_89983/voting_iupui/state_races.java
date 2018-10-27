0855ecb436980612a23d4b9c265cb18440888b35
garrett <garrett7621@gmail.com>
2018-10-27 12:53:11 -0400

    state races

--- app/src/main/java/com/example/c_89983/voting_iupui/state_races.java
+++ app/src/main/java/com/example/c_89983/voting_iupui/state_races.java
@@ -7,6 +7,6 @@ public class state_races extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.state_races);
     }
 }
